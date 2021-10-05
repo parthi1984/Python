f58aec7c44bf5f81e29abcd53de1c4f505ef0d6e
parthibanp <parthiban.1984@gmail.com>
2021-10-05 22:03:45 +0800

    Added config properties file and logic

--- UrlShortcutService/TestService.py
+++ UrlShortcutService/TestService.py
@@ -1,18 +1,31 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+#!/usr/bin/env python3.9
 
-url_dict = {"google": "https://www.google.com"}
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import configparser
 
 
 class Serv(BaseHTTPRequestHandler):
 
+    url_dict = {}
+
+    def load_config(self):
+        config = configparser.RawConfigParser()
+        config.read('config.properties')
+        self.url_dict = config['UrlDictionary']
+
     def do_GET(self):
+        # Load urls only once
+        if self.url_dict.__len__() == 0:
+            self.load_config()
+            print(self.url_dict.keys())
+
         if self.path == '/':
             self.path = '/test.html'
         try:
-            print(self.path[1:])
-            if self.path[1:] in url_dict.keys():
+            # If shortcut is available in url dictionary then Redirect to it's URL
+            if self.path[1:] in self.url_dict.keys():
                 self.send_response(301)
-                self.send_header('Location', url_dict[self.path[1:]])
+                self.send_header('Location', self.url_dict[self.path[1:]])
                 self.end_headers()
             else:
                 file_to_open = open(self.path[1:]).read()
@@ -28,5 +41,5 @@ class Serv(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    httpd = HTTPServer(('localhost', 8080), Serv)
+    httpd = HTTPServer(('go', 8080), Serv)
     httpd.serve_forever()
