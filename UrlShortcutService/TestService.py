a8199e5f50c59201089802c37dcd946b32be54e6
parthibanp <parthiban.1984@gmail.com>
2021-10-14 21:41:51 +0800

    14Oct21 commit
    HTML file enrichment and POST handling

--- UrlShortcutService/TestService.py
+++ UrlShortcutService/TestService.py
@@ -2,6 +2,7 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import configparser
+import logging
 
 
 class Serv(BaseHTTPRequestHandler):
@@ -17,7 +18,6 @@ class Serv(BaseHTTPRequestHandler):
         # Load urls only once
         if self.url_dict.__len__() == 0:
             self.load_config()
-            print(self.url_dict.keys())
 
         if self.path == '/':
             self.path = '/test.html'
@@ -39,7 +39,21 @@ class Serv(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        # Prepare & send response message
+        self.path = '/test.html'
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes("POST request for {}".format(self.path), 'utf-8'))
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     httpd = HTTPServer(('go', 8080), Serv)
     httpd.serve_forever()
