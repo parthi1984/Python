b40c66caa3a6f124cf4a29da58b0a69d53f1df79
parthibanp <parthiban.1984@gmail.com>
2021-12-07 11:16:44 +0800

    first commit
    open browser, open files - voice controlled

---
+++ speech_recog/voice_browse.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+import webbrowser
+
+speech = sr.Recognizer()
+
+
+def voice_to_text():
+    voice_input = ""
+    with sr.Microphone() as source:
+        speech.adjust_for_ambient_noise(source)
+        try:
+            audio = speech.listen(source)
+            voice_input = speech.recognize_google(audio)
+        except sr.UnknownValueError:
+            pass
+        except sr.RequestError:
+            pass
+        except sr.WaitTimeoutError:
+            pass
+    return voice_input
+
+
+while True:
+    print('Python is listening...')
+    inp = voice_to_text()
+    print(f'You just said: {inp}')
+
+    if inp == 'stop listening':
+        print('Goodbye!')
+        break
+    elif "browser" in inp:
+        inp = inp.replace('browser ', '')
+        webbrowser.open("http://"+inp)
+        continue
